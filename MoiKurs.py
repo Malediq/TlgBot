6de345cc39d4eb056249fc192d9b0ed15e78aab4
Artem Zabolotskiy <110981706+Malediq@users.noreply.github.com>
2023-01-11 22:05:28 +1000

    Update MoiKurs.py

--- MoiKurs.py
+++ MoiKurs.py
@@ -25,27 +25,3 @@ def moikurs(message):
     except:
         x=x
     return x
-
-
-def moikurs2(message):
-    myheaders = {}
-    myheaders['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
-    source = requests.get("https://ru.myfin.by/bank/vtb/currency", headers=myheaders)
-    html = source.text
-    x = True
-    try:
-        kursP = float(html[(html.find('Время обновления</th></tr><tr><td class="title"><a href="/currency/usd">Доллар</a></td><td>')+91):(html.find('Время обновления</th></tr><tr><td class="title"><a href="/currency/usd">Доллар</a></td><td>') + 96)])
-        kursP -= 5.4
-        raznica = abs(kursP - 65.72)
-        if kursP > 65.72:
-            bot.send_message(message.chat.id, str(kursP) + '(+' + str(round(raznica, 2)) + ')')
-            bot.send_message(message.chat.id, 'Прибыль составит:\t' + str(round(raznica, 2) * 1111))
-        elif kursP == 65.72:
-            bot.send_message(message.chat.id, kursP)
-        else:
-            bot.send_message(message.chat.id, str(kursP) + '(-' + str(round(raznica, 2)) + ')')
-            bot.send_message(message.chat.id, 'Убыток составит: ' + str(round(raznica, 2) * 1111))
-        x=False
-    except:
-        x=x
-    return x
