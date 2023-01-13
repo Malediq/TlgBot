3fe3b7375d79c3f03acb3febd71970f1eed1fdc5
Artem Zabolotskiy <110981706+Malediq@users.noreply.github.com>
2023-01-13 18:43:29 +1000

    v3.0

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+echo "Enter filepath to TlgBot folder"
+read Filepath
+cd $Filepath
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt-get update
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt --fix-broken install -y
 sudo apt-get install -y python3-pip
 sudo apt-get install -y unzip
 pip3 install pyTelegramBotAPI
@@ -7,8 +13,13 @@ pip3 install bs4
 pip3 install lxml
 pip3 install selenium
 pip3 install geopy
-cd /home
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-wget https://chromedriver.storage.googleapis.com/108.0.5359.71/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip -d /home
+sed -i 's|<PATH>|$Filepath|' bot.service
+cp bot.service /etc/systemd/system/bot.service
+chmod 664 /etc/systemd/system/bot.service
+sed -i 's|/home|$Filepath|g' value.py
+echo "Enter your Bot id"
+read Botid
+sed -i 's|telebot.TeleBot('')|telebot.TeleBot('$Botid')|' value.py
+service bot start
+service bot status
+google-chrome-stable --version
